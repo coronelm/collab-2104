242ddffc244db2dfd736cd1b939879b4815e41d5
kristamourine <kristamourinebarza@gmail.com>
2025-11-26 18:58:21 +0800

    Krista: Inserted enrollments data

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE enrollments ( enrollment_id INT PRIMARY KEY, student_id INT, class_id INT );
-INSERT INTO enrollments (enrollment_id, saddtudent_id, class_id) VALUES (1, 1001, 2001);
+INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 1001, 2001);
+INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (2, 1002, 2002);
+INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (3, 1003, 2003);
