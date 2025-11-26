b5295f74cb49e8aae303691256480970211b5ffd
Daee0606 <1022Drake@gmail.com>
2025-11-26 23:06:07 +0800

    Drake: Added school_year column

--- schema.sql
+++ schema.sql
@@ -4,5 +4,4 @@ CREATE TABLE enrollments ( enrollment_id INT PRIMARY KEY, student_id INT, class_
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 1001, 2001);
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (2, 1002, 2002);
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (3, 1003, 2003);
-ALTER TABLE enrollments
-ADD school_year VARCHAR(20);
+ALTER TABLE enrollments ADD school_year VARCHAR(20);
