52a612633182ef1ed63520ea2e8af1c25df38064
rimu.exe <jeromedimalaluan30@gmail.com>
2025-11-26 16:28:27 +0800

    Robles, Elmar Jerome T.

--- students.sql
+++ students.sql
@@ -1,2 +1,3 @@
 CREATE TABLE students (id INT PRIMARY KEY, name VARCHAR(50), grade_level INT);
 INSERT INTO students (id, name, grade_level) VALUES (1,'Ana',10),(2,'Mark',11),(3,'Liza',12);
+ALTER TABLE students ADD age INT;
