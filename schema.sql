26f08c4cb51b9a0dedfea82f411bd510cae3a355
kristamourine <kristamourinebarza@gmail.com>
2025-11-26 18:41:06 +0800

    Krista: Inserted sample enrollments

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE school_db; USE school_db
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 1001, 2001)
+CREATE DATABASE school_db; USE school_db;
+CREATE TABLE enrollments ( enrollment_id INT PRIMARY KEY, student_id INT, class_id INT );
+INSERT INTO enrollments (enrollment_id, saddtudent_id, class_id) VALUES (1, 1001, 2001);
