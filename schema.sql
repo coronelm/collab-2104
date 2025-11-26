ae279b37b46aa79f3af835a7d3651807e4358c38
Matthew1806 <matthewcudala@gmail.com>
2025-11-26 19:12:43 +0800

    Group10 - Ungui, Erica Anne: Insert 3 fee records

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE fees (fee_id INT PRIMARY KEY, student_id INT, amount DECIMAL(10,2));
+INSERT INTO fees (student_id, amount) VALUES (101, 1500.00);
+INSERT INTO fees (student_id, amount) VALUES (102, 2000.00);
+INSERT INTO fees (student_id, amount) VALUES (103, 1750.50);
