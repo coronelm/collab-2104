5c7d102444375ec1e7395cf0d3e8c23a8c83a304
iamjoshuaromero <iam.joshuaromero@gmail.com>
2025-11-26 11:27:23 +0800

    Marquez added head_teacher column

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE departments (dept_id INT, dept_name VARCHAR(50));
 INSERT INTO departments VALUES (1, 'CICS');
 INSERT INTO departments VALUES (2, 'CABE');
 INSERT INTO departments VALUES (3, 'CAS');
+ALTER TABLE departments ADD head_teacher VARCHAR(50);
