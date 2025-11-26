b128865a508e90ded3b38964b1e01f8b5eaceee4
iamjoshuaromero <iam.joshuaromero@gmail.com>
2025-11-26 11:24:22 +0800

    Ruben inserted sample departments

--- schema.sql
+++ schema.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE school_db; USE school_db;
 CREATE TABLE departments (dept_id INT, dept_name VARCHAR(50));
+INSERT INTO departments VALUES (1, 'CICS');
+INSERT INTO departments VALUES (2, 'CABE');
+INSERT INTO departments VALUES (3, 'CAS');
