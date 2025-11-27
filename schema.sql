f12b78c607f6e87e11b68edd3fb4fdcafb625dc0
lain-0 <lalaineanne0923@gmail.com>
2025-11-28 00:22:38 +0800

    Doubled Insert
    Fixed syntax errors in attendance insert statements.

--- schema.sql
+++ schema.sql
@@ -13,6 +13,4 @@ CREATE TABLE attendance(attendance_id INT PRIMARY KEY, student_id INT, date DATE
 INSERT INTO attendance(attendance_id, student_id, date, status) VALUES (0001,0001, 2025-11-28, 'present'), (003,003, 2025-11-30, 'present');
 INSERT INTO attendance(attendance_id, student_id, date, status) VALUES (0001, 01, 2025-11-30, 'Present') (0002, 02, 2025-11-29, 'Present'), (0003, 03, 2025-11-30, 'Present');
 =======
-CREATE TABLE attendance(attendance_id INT PRIMARY KEY, student_id INT, date DATE, status ENUM('present', 'absent') NOT NULL DEFAULT 'present');
->>>>>>> 83f52e97e137f6cedd751f6378c116273b5b4a62
-INSERT INTO attendance(attendance_id, student_id, date, status) VALUES(0001, 01, 2025-11-28, 'Present'), (0002, 02, 2025-11-29, 'Present'), (0003, 03, 2025-11-30, 'Present');
+
