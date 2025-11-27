bc61ebf7f1295434df40f1b046809bcc92e03000
mayciii <sigriddimaano@gmail.com>
2025-11-27 22:12:21 +0800

    Group 7 - Dimaano,May Sigrid: Create subjects table

--- schema.sql
+++ schema.sql
@@ -5,3 +5,5 @@ INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 1001, 2
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (2, 1002, 2002);
 INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (3, 1003, 2003);
 ALTER TABLE enrollments ADD school_year VARCHAR(20);
+CREATE DATABASE school_db; USE school_db
+CREATE TABLE subjects ( subject_id INT PRIMARY KEY, subject_name VARCHAR(100));
