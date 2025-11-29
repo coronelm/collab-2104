7fadd5e42da6d66a1e2e8cf3432cd022f9aff65d
elishaclaire <elishabrondialtuscano14@gmail.com>
2025-11-29 14:38:25 +0800

    Remove schema.sql from main

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE school_db; USE school_db;
-CREATE DATABASE school_db; USE school_db;
-CREATE TABLE enrollments ( enrollment_id INT PRIMARY KEY, student_id INT, class_id INT );
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (1, 1001, 2001);
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (2, 1002, 2002);
-INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (3, 1003, 2003);
-ALTER TABLE enrollments ADD school_year VARCHAR(20);
