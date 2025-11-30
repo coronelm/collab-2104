3e85a0cdeeff770b80aa77ebc9de375e989f9cfe
Jazztin18 <24-31805@g.batstate-u.edu.ph>
2025-11-30 19:09:04 +0800

    Group 7 - Perez, Jazztin: Add units columns

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db; USE school_db
 CREATE TABLE subjects ( subject_id INT PRIMARY KEY, subject_name VARCHAR(100));
 INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Computer Programming'), (2, 'OOP'), (3, 'Computer Networking');
+ALTER TABLE subjects ADD COLUMN units INT;
