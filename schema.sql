a9ecae4cf724b082a1e4d99c91140f2132d99709
aic-fth <24-37186@g.batstate-u.edu.ph>
2025-11-30 18:56:48 +0800

    Group 7 - Valencia, Faith: Insert subjects

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db; USE school_db
 CREATE TABLE subjects ( subject_id INT PRIMARY KEY, subject_name VARCHAR(100));
+INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Computer Programming'), (2, 'OOP'), (3, 'Computer Networking');
