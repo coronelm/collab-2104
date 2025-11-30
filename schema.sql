11aaa8c42384c9d4d82d45b12a7033eb47e3e101
dekann101 <24-38290@g.batstate-u.edu.ph>
2025-11-30 15:38:53 +0800

    Group 7 - Pangilinan, Dean: Update one subjects unit.

--- schema.sql
+++ schema.sql
@@ -21,3 +21,4 @@ INSERT INTO subjects (subject_id, subject_name) VALUES (1, 'Computer Programming
 ALTER TABLE subjects ADD COLUMN units INT;
 >>>>>>> d4e5720116c4a7b459c8f896c32d440222e03e9f
 UPDATE subjects SET units = 3 WHERE subject_id = 2;
+UPDATE subjects SET units = 3 WHERE subject_id = 2;
