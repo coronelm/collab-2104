d57612847acbfc23b7e383c4641de86d3b6cd520
king777-afk <xshadowx012@gmail.com>
2025-11-30 21:49:32 +0800

    Group3-MCGruta: added teacher_id and FK column

--- schema.sql
+++ schema.sql
@@ -7,3 +7,5 @@ INSERT INTO enrollments (enrollment_id, student_id, class_id) VALUES (3, 1003, 2
 ALTER TABLE enrollments ADD school_year VARCHAR(20);
 CREATE TABLE classes ( id INT, class_name VARCHAR(50), room_no INT );
 INSERT INTO classes (id, class_name, room_no) VALUES(01,'BSIT-2104',1111), (02,'BSIT-2105',2222), (03,'BSIT-2106',3333);
+ALTER TABLE classes ADD COLUMN teacher_id INT;
+ALTER TABLE classes ADD CONSTRAINT fk_classes_teacher FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id);
